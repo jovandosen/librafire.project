e54e8335d6074314d30a15b954dcc4cc14e3a555
jovandosen <jovandosen1994@gmail.com>
2019-11-25 17:05:16 +0100

    javascript validation on register page added

--- public/js/register.js
+++ public/js/register.js
@@ -4,6 +4,10 @@ $(document).ready(function(){
 		showHidePassword();
 	});
 
+	$("#register-button").on("click", function(){
+		validateRegisterData();
+	});
+
 });
 
 function showHidePassword()
@@ -27,6 +31,124 @@ function showHidePassword()
 		}
 
 		passwordEye.type = "password";
-		
+
 	}
+}
+
+function validateRegisterData()
+{
+	var firstName = $("#first-name").val();
+	var lastName = $("#last-name").val();
+	var email = $("#email").val();
+	var password = $("#password").val();
+
+	var error = false;
+	var firstNameError = '';
+	var lastNameError = '';
+	var emailError = '';
+	var passwordError = '';
+
+	if( firstName == '' ){
+		error = true;
+		firstNameError = 'First Name can not be empty.';
+		$("#first-name-error-container p").text(firstNameError);
+		$("#first-name").addClass("error-border");
+	} else {
+
+		var firstNameLength = firstName.length;
+
+		if( firstNameLength < 3 ){
+			error = true;
+			firstNameError = 'First Name must have at least 3 characters.';
+			$("#first-name-error-container p").text(firstNameError);
+			$("#first-name").addClass("error-border");
+		}
+	}
+
+	if( firstNameError == '' ){
+		$("#first-name-error-container p").text(firstNameError);
+		if( $("#first-name").hasClass("error-border") ){
+			$("#first-name").removeClass("error-border");
+		}
+	}
+
+	if( lastName == '' ){
+		error = true;
+		lastNameError = 'Last Name can not be empty.';
+		$("#last-name-error-container p").text(lastNameError);
+		$("#last-name").addClass("error-border");
+	} else {
+
+		var lastNameLength = lastName.length;
+
+		if( lastNameLength < 3 ){
+			error = true;
+			lastNameError = 'Last Name must have at least 3 characters.';
+			$("#last-name-error-container p").text(lastNameError);
+			$("#last-name").addClass("error-border");
+		}
+	}
+
+	if( lastNameError == '' ){
+		$("#last-name-error-container p").text(lastNameError);
+		if( $("#last-name").hasClass("error-border") ){
+			$("#last-name").removeClass("error-border");
+		}
+	}
+
+	if( email == '' ){
+		error = true;
+		emailError = 'Email can not be empty.';
+		$("#email-error-container p").text(emailError);
+		$("#email").addClass("error-border");
+	} else {
+
+		if( validateEmailAddress(email) === false ){
+			error = true;
+			emailError = 'Email address is not valid.';
+			$("#email-error-container p").text(emailError);
+			$("#email").addClass("error-border");
+		}
+
+	}
+
+	if( emailError == '' ){
+		$("#email-error-container p").text(emailError);
+		if( $("#email").hasClass('error-border') ){
+			$("#email").removeClass('error-border');
+		}
+	}
+
+	if( password == '' ){
+		error = true;
+		passwordError = 'Password can not be empty.';
+		$("#password-error-container p").text(passwordError);
+		$("#password").addClass("error-border");
+	} else {
+
+		passwordLength = password.length;
+
+		if( passwordLength < 5 ){
+			error = true;
+			passwordError = 'Password must have at least 5 characters.';
+			$("#password-error-container p").text(passwordError);
+			$("#password").addClass("error-border");
+		}
+
+	}
+
+	if( passwordError == '' ){
+		$("#password-error-container p").text(passwordError);
+		if( $("#password").hasClass("error-border") ){
+			$("#password").removeClass("error-border");
+		}
+	}
+
+}
+
+function validateEmailAddress(email)
+{
+	var regularEx = /\S+@\S+\.\S+/;
+
+	return regularEx.test(email);
 }
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -10,43 +10,43 @@
 
 			<div id="first-name-container">
 				<div id="first-name-label-container">
-					<label for="first-name"><i class="fa fa-user icon"></i> First Name:</label>
+					<label for="first-name"><i class="fa fa-user icon"></i> <font color="red">*</font>First Name:</label>
 				</div>
 				<div id="first-name-field-container">
-					<input type="text" name="first-name" id="first-name" placeholder="Enter your first name..." />
+					<input type="text" name="first-name" id="first-name" placeholder="Enter your first name..." minlength="3" autocomplete="off" />
 				</div>
-				<div id="first-name-error-container">test</div>
+				<div id="first-name-error-container"><p></p></div>
 			</div>
 
 			<div id="last-name-container">
 				<div id="last-name-label-container">
-					<label for="last-name"><i class="fa fa-user icon"></i> Last Name:</label>
+					<label for="last-name"><i class="fa fa-user icon"></i> <font color="red">*</font>Last Name:</label>
 				</div>
 				<div id="last-name-field-container">
-					<input type="text" name="last-name" id="last-name" placeholder="Enter your last name..." />
+					<input type="text" name="last-name" id="last-name" placeholder="Enter your last name..." minlength="3" autocomplete="off" />
 				</div>
-				<div id="last-name-error-container">test</div>
+				<div id="last-name-error-container"><p></p></div>
 			</div>
 
 			<div id="email-container">
 				<div id="email-label-container">
-					<label for="email"><i class="fa fa-envelope icon"></i> Email:</label>
+					<label for="email"><i class="fa fa-envelope icon"></i> <font color="red">*</font>Email:</label>
 				</div>
 				<div id="email-field-container">
-					<input type="text" name="email" id="email" placeholder="Enter your email address..." />
+					<input type="text" name="email" id="email" placeholder="Enter your email address..." autocomplete="off" />
 				</div>
-				<div id="email-error-container">test</div>
+				<div id="email-error-container"><p></p></div>
 			</div>
 
 			<div id="password-container">
 				<div id="password-label-container">
-					<label for="password"><i class="fa fa-key icon"></i> Password:</label>
+					<label for="password"><i class="fa fa-key icon"></i> <font color="red">*</font>Password:</label>
 				</div>
 				<div id="password-field-container">
-					<input type="password" name="password" id="password" placeholder="Enter your password..." />
+					<input type="password" name="password" id="password" placeholder="Enter your password..." minlength="5" />
 					<i class="fa fa-eye-slash" aria-hidden="true" id="password-eye"></i>
 				</div>
-				<div id="password-error-container">test</div>
+				<div id="password-error-container"><p></p></div>
 			</div>
 
 			<div id="register-button-container">
